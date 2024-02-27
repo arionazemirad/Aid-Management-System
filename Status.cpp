e09b91fe9a35bfadef004a232611d252d8a79487
arionazemirad <141854484+arionazemirad@users.noreply.github.com>
2024-02-26 23:14:55 -0500

    Update Status.cpp

--- Status.cpp
+++ Status.cpp
@@ -1,10 +1,4 @@
-/*
-Name: Ario Nazemirad
-Email: anazemirad@myseneca.ca
-ID: 144647299
-Date: Dec 3
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*/
+//Ario Nazemirad
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
