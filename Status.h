dbe2d8b492c168cc651530e05e92fa4e24f2c8ec
arionazemirad <141854484+arionazemirad@users.noreply.github.com>
2024-02-26 23:15:02 -0500

    Update Status.h

--- Status.h
+++ Status.h
@@ -1,10 +1,4 @@
-/*
-Name: Ario Nazemirad
-Email: anazemirad@myseneca.ca
-ID: 144647299
-Date: Dec 3
-I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-*/
+//Ario Nazemirad
 #ifndef STATUS_H
 #define STATUS_H
 
